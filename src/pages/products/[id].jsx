97ab2c50fb0023da3f6bacaa0ec800d6b73a6846
Daniel Spykstra <dspyk77@gmail.com>
2023-11-11 16:18:30 -0600

    WIP

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
 
 function Page() {
   const [product, setProduct] = useState(null);
@@ -8,6 +11,7 @@ function Page() {
   const { id } = router.query;
 
   useEffect(() => {
+    console.log('Current ID:', id);
     const fetchProduct = async () => {
       try {
         // Check if id is truthy (not null or undefined) before making the request
@@ -34,19 +38,71 @@ function Page() {
     fetchProduct();
   }, [id]);
 
+  const handleDelete = async () => {
+    const confirmation = window.confirm('Are you sure you want to delete this product?');
+
+    if (confirmation && id) {
+      try {
+        const response = await fetch(`/api/products/${id}`, {
+          method: 'DELETE'
+        });
+
+        if (response.ok) {
+          // If deletion is successful, redirect to the product list page
+          router.push('/products');
+        } else {
+          console.error(response);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  };
+
   return (
     <>
       <h1>Product</h1>
 
       {product ? (
         <>
-          <p>Name: {product.name}</p>
-          <p>Category: {product.category}</p>
-          <p>Price: {product.price}</p>
-          <p>Weight: {product.weight}</p>
+          <Button variant="dark" className="mb-4 me-auto" href="/products">Back</Button>
+
+          <Table variant='dark' size="sm" responsive className="show-table">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Name:</td>
+                <td>{product.name}</td>
+              </tr>
+              <tr>
+                <td>Category:</td>
+                <td>{product.category}</td>
+              </tr>
+              <tr>
+                <td>Price:</td>
+                <td>{product.price}</td>
+              </tr>
+              <tr>
+                <td>Weight:</td>
+                <td>{product.weight}</td>
+              </tr>
+            </tbody>
+          </Table>
+
+          <Link href="">Edit</Link>
+
+          <span> | </span>
+
+          <a href="#" onClick={handleDelete}>Delete</a>
         </>
       ) : (
-        <p>Loading...</p>
+        <h4>Loading...</h4>
       )}
     </>
   );
